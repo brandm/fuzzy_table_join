596df5ad0c329d0621b8f03f6d376cd56a647045
Michael Brand <michael.ch.brand@gmail.com>
2017-03-03 12:00:00 +0100

    Version 2

--- fuzzy_table_join.py
+++ fuzzy_table_join.py
@@ -14,9 +14,9 @@ difflib.get_close_matches in Python 3.
 #     Python live coding.
 #   - Supports Emacs Outshine mode.
 #   - TODO
-#     - [ ] df1_mutation[on] seems wrong.
-#       - [ ] Annotate new column for closest match.
-#       - [ ] join_fuzzy(df1, field1, df2, field2, cutoff)
+#     - [-] df1_mutation[on] seems wrong.
+#       - [X] Annotate new column for closest match.
+#       - [X] join_fuzzy(df1, field1, df2, field2, cutoff)
 #       - [ ] Allow no match.
 #       - [ ] Annotate new column for alternate matches.
 #       - [ ] Annotate new column for number of matches.
@@ -28,6 +28,8 @@ difflib.get_close_matches in Python 3.
 # * Setup
 import difflib
 import pandas as pd
+import random
+import string
 
 # * Input data for test
 _df1 = pd.DataFrame(
@@ -35,29 +37,33 @@ _df1 = pd.DataFrame(
         ['a1', "Foo1"],
         ['b1', "Bar1"],
     ],
-    columns=["Id1", "CName"])
+    columns=["Id1", "Name1"])
 
 _df2 = pd.DataFrame(
     [
         ['a2', "Bar2"],
         ['b2', "Foo2"],
     ],
-    columns=["Id2", "CName"])
+    columns=["Id2", "Name2"])
 
 
 # * Functions
-def join_fuzzy(df1, df2, on, cutoff):
-    df1_mutation = df2.copy()
-    df1_mutation[on] = df1_mutation[on].map(
+def join_fuzzy(df1, field1, df2, field2, cutoff):
+    df1_mutation = df1.copy()
+    random.seed(20170323)
+    df1_matched_name = "Matched2-" + ''.join(
+        random.choice(string.ascii_letters) for _ in range(5))
+    df1_mutation[df1_matched_name] = df1_mutation[field1].map(
         lambda cell:
-        difflib.get_close_matches(cell, df1[on], n=1, cutoff=cutoff)[0])
-    return df1.merge(df1_mutation, how='outer', on=on)
+        difflib.get_close_matches(cell, df2[field2], n=1, cutoff=cutoff)[0])
+    return df1_mutation.merge(
+        df2, how='outer', left_on=df1_matched_name, right_on=field2)
 
 
 # * Main
 print(_df1, '\n')
 print(_df2, '\n')
-print(join_fuzzy(_df1, _df2, "CName", 0.7))
+print(join_fuzzy(_df1, "Name1", _df2, "Name2", 0.7))
 
 
 # * Live coding
